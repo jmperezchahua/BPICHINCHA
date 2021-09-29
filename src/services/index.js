21e4dba57b71c86d267f804d5e3efe3aebb7af4e
jperez <jmperezchahua@gmail.com>
2021-09-29 15:57:29 -0500

    depura

--- src/services/index.js
+++ src/services/index.js
@@ -1,22 +1,12 @@
 import axios from "axios";
 
-const Serie = (filterInput) => {
+const Serie = () => {
   const api = `https://rickandmortyapi.com/api/character`;
-  return (
-    axios
-      .get(api)
-      .then((res) => res.data)
-      // .then((data) =>
-      //   filterInput.length
-      //     ? data.results.filter(
-      //         (ele) =>
-      //           ele.name.toLowerCase().indexOf(filterInput.toLowerCase()) >= 0
-      //       )
-      //     : data.results
-      // )
-      .then((data) => data.results)
-      .catch((err) => console.log("Horror ...", err))
-  );
+  return axios
+    .get(api)
+    .then((res) => res.data)
+    .then((data) => data.results)
+    .catch((err) => console.log("Horror ...", err));
 };
 
 export default Serie;
